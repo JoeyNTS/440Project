09db11cf8615815ff72e755d4e7fafc625a4c9c1
JoeyNTS <70410870+JoeyNTS@users.noreply.github.com>
2023-11-09 09:10:40 -0800

    done

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,1 +1,94 @@
 
+from collections import namedtuple
+
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
+class TicTacToe:
+
+    def __init__(self, h = 3, v = 3, k = 3):
+        self.h = h
+        self.v = v
+        self.k = k
+        moves = [(x, y) for x in range(1, h + 1)
+                 for y in range(1, v + 1)]
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
+
+    def actions(self, state):
+        """Legal moves are any square not yet taken."""
+        return state.moves
+
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board, moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == 'X' else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+    def display(self, state):
+        board = state.board
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +1 if player == 'X' else -1
+        else:
+            return 0
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        """Return true if there is a line through move on board for player."""
+        (delta_x, delta_y) = delta_x_y
+        x, y = move
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1  # Because we counted move itself twice
+        return n >= self.k
+    
+    def display(self, state):
+        """Print or otherwise display the state."""
+        print(state)
+
+if __name__ == "__main__":
+ 
+    game = TicTacToe()
+    state = game.initial
+
+    print("\nLet's play TicTacToe!\n")
+    game.display(state)
+
+    while not game.terminal_test(state):
+        player = state.to_move
+        print(f"\n{player}'s turn.")
+        move = eval(input("Enter your move as (x, y): "))
+        state = game.result(state, move)
+        game.display(state)
+
+    print("\nGame over.")
+    if state.utility == 1:
+        print("X wins!")
+    elif state.utility == -1:
+        print("O wins!")
+    else:
+        print("It's a tie!")
